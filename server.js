44698ccd3a7bac259bc93af8742f96e39c39129b
CDA <cathyda97@gmail.com>
2022-09-14 23:30:36 -0400

    Version 1.1 - Completed

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const connect = require('connect');
 const app = connect();
 
-function helloWorldPlain(req, res, next){
+function helloWorldPlain(req, res){
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 };
 
-function helloWorldHTML(req, res, next){
+function helloWorldHTML(req, res){
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hellow from NodeJS Application as html</h1>');
 }
 
-function helloWorldJSON(req, res, next){
+function helloWorldJSON(req, res){
     res.setHeader('Content-Type', 'application/json');
-    res.end({
-        "message":"Hello from NodeJS Application as json"
-    });
+    res.end('{"Message":"Hello from NodeJS Application as json"}');
 }
 
 app.use('/html', helloWorldHTML);
